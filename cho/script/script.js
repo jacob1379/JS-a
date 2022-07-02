689074d8a5120d01c4eb48d006c49ee44955ea12
조준형 <hompajo27@naver.com>
2022-07-02 12:58:18 +0900

    new

--- cho/script/script.js
+++
@@ -1,6 +0,0 @@
-const init = () => {
-    $('header').load('/cho/fragment/header.html');
-    $('nav').load('/cho/fragment/nav.html');
-    $('aside').load('/cho/fragment/aside.html');
-    $('footer').load('/cho/fragment/footer.html');
-}
---
+++ done/script/script.js
@@ -0,0 +1,6 @@
+const init = () => {
+    $('header').load('/done/fragment/header.html')
+    $('nav').load('/done/fragment/nav.html')
+    $('aside').load('/done/fragment/aside.html')
+    $('footer').load('/done/fragment/footer.html')
+}
